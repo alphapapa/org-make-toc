53e793cb6aa8f66704d615d51e828e91ec4061e0
Adam Porter <adam@alphapapa.net>
2018-05-30 21:36:45 -0500

    Prepare for package release

--- org-make-toc.el
+++ org-make-toc.el
@@ -1,3 +1,46 @@
+;;; org-make-toc.el --- Automatic tables of contents for Org files  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Adam Porter
+
+;; Author: Adam Porter <adam@alphapapa.net>
+;; URL: http://github.com/alphapapa/org-make-toc
+;; Version: 0.1-pre
+;; Package-Requires: ((emacs "25.1") (dash "2.12") (s "1.10.0") (org "9.0"))
+;; Keywords: Org, convenience
+
+;;; Commentary:
+
+;; This package makes it easy to have a customizable table of contents in Org files that can be
+;; updated manually, or automatically when the file is saved.  Links to headings are created
+;; compatible with GitHub's Org renderer.
+
+;;;; Installation
+
+;; Install the packages `dash' and `s'.  Then put this file in your `load-path', and put this in
+;; your init file:
+
+;; (require 'org-make-toc)
+
+;;;; Usage
+
+;; 1.  Make a heading in the Org file where you want the table of contents, and give it the Org
+;; property "TOC" with the value "this".
+
+;; 2.  Run the command `org-make-toc-make-toc'.
+
+;;;;; Customization
+
+;; The table of contents can be customized by setting the "TOC" property of headings:
+
+;; +  Set to "ignore" to omit a heading from the TOC.
+;; +  Set to "ignore-children" or "0" to omit a heading's child headings from the TOC.
+;; +  Set to a number N to include child headings no more than N levels deep in the TOC.
+
+;;;;; Automatically update on save
+
+;; To automatically update a file's TOC when the file is saved, use the command
+;; `add-file-local-variable' to add `org-make-toc-make-toc' to the Org file's `before-save-hook'.
+
 ;;; License:
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -17,6 +60,8 @@
 
 (require 'cl-lib)
 (require 'org)
+(require 'subr-x)
+
 (require 'dash)
 (require 's)
 
@@ -33,11 +78,10 @@
   "Make or update table of contents in current buffer."
   (interactive)
   (when-let ((toc-position (or (org-find-property "TOC" "this")
-                               (when (called-interactively-p)
-                                 ;; Don't error unless the user called this function manually,
-                                 ;; e.g. if it's added to the before-save-hook in all Org mode
-                                 ;; files, don't error just because a file doesn't have a TOC.
-                                 (user-error "No TOC node found.  A node must have the \"TOC\" property set to \"this\""))))
+                               (let ((message "No TOC node found.  A node must have the \"TOC\" property set to \"this\""))
+                                 (if (called-interactively-p 'interactive)
+                                     (message message)
+                                   (user-error message)))))
              (list (or (->> (cddr (org-element-parse-buffer 'headline))
                             (org-make-toc--remove-ignored-entries)
                             (org-make-toc--remove-higher-level-than-toc)
@@ -47,16 +91,17 @@
 
 ;;;; Functions
 
-(defun org-walk-tree (tree element-pred)
+(defun org-make-toc--walk-tree (tree element-pred)
+  "Walk TREE and collect elements matching ELEMENT-PRED."
   (cl-loop for element in tree
            when (eql 'headline (car element))
            when (funcall element-pred element)
            collect (list 'headline
                          :name (org-element-property :title element)
-                         :children (org-walk-tree (cdddr element) element-pred))))
+                         :children (org-make-toc--walk-tree (cdddr element) element-pred))))
 
 (defun org-make-toc--filter-tree (tree pred)
-  "Return tree with elements for which PRED returns non-nil."
+  "Return TREE with elements for which PRED returns non-nil."
   (cl-loop with properties
            with children
            for element in tree
@@ -142,31 +187,22 @@
                                                 #'org-make-toc--element-level)))
     (org-make-toc--filter-tree tree (lambda (element)
                                       (>= (org-element-property :level element)
-                                         toc-level)))))
+                                          toc-level)))))
 
 ;;;;; Predicates
 
-(defun org-make-toc--include-heading-p (element)
-  "Return non-nil if heading should be included in ToC."
-  (pcase (org-element-property :TOC element)
-    ("ignore" nil)
-    ('nil t)
-    (other (user-error "Invalid value for TOC property at position %s: "
-                       (point) other))))
-
 (defun org-make-toc--toc-entry-p (element)
-  (org-element-property :title element)
+  "Return non-nil if ELEMENT is the table of contents."
   (string= "this" (org-element-property :TOC element)))
 
 ;;;;; Transformer
 
 (defun org-make-toc--tree-to-list (tree)
+  "Return TREE converted to a table of contents as a plain list."
   (let* ((contents (s-join "\n"
-                           (cl-loop with toc-level = (org-make-toc--toc-level tree)
-                                    for element in tree
+                           (cl-loop for element in tree
                                     for level = (or (org-element-property :level element) 0)
                                     for indent = (s-repeat (* 2 level) " ")
-                                    for title = (org-element-property :title element)
                                     for children = (org-make-toc--tree-to-list (caddr element))
                                     for link = (org-make-toc--link-entry-github element)
                                     collect (concat indent "-" "  " link "\n" children)))))
@@ -196,13 +232,15 @@
 
 ;;;;; Misc
 
-(defun org-make-toc--element-level (element)
-  (org-element-property :level element))
-
 (defun org-make-toc--toc-level (tree)
+  "Return the outline level of the table of contents in TREE."
   (org-make-toc--first-in-tree tree
                                #'org-make-toc--toc-entry-p
                                #'org-make-toc--element-level))
+
+(defun org-make-toc--element-level (element)
+  "Return the outline level of Org element ELEMENT."
+  (org-element-property :level element))
 
 (defun org-make-toc--replace-entry-contents (pos contents)
   "Replace the contents of entry at POS with CONTENTS."
@@ -215,3 +253,5 @@
       (setf (buffer-substring (point) end) contents))))
 
 (provide 'org-make-toc)
+
+;;; org-make-toc.el ends here
