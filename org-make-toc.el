5cff9cd9172b1bcfafb1fae4196a99c29bb2a096
Adam Porter <adam@alphapapa.net>
2018-06-08 16:20:46 -0500

    Tag: 0.1

--- org-make-toc.el
+++ org-make-toc.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: http://github.com/alphapapa/org-make-toc
-;; Version: 0.1-pre
+;; Version: 0.1
 ;; Package-Requires: ((emacs "25.1") (dash "2.12") (s "1.10.0") (org "9.0"))
 ;; Keywords: Org, convenience
 
